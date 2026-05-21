a8064a7582553975f927860719e771a4057f231b
AJDR02 <aj_rodriguez2@hotmail.com>
2026-05-20 23:15:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function mostrarMensaje(){
+
+    document.getElementById("titulo").innerHTML =
+    "Bienvenido a Azure ☁️";
+
+    document.getElementById("descripcion").innerHTML =
+    "Azure permite desplegar aplicaciones web, bases de datos, inteligencia artificial y servicios en la nube.";
+}
+
+function volverInicio(){
+
+    document.getElementById("titulo").innerHTML =
+    "Microsoft Azure";
+
+    document.getElementById("descripcion").innerHTML =
+    "Mi primera aplicación web desplegada en Microsoft Azure.";
+}
